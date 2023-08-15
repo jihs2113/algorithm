60adbbffceb9d6fcf3708f8ca620c2f7696ee5df
김지환 <jihwan-morning@mz.co.kr>
2023-08-15 23:19:59 +0900

    add even odd

---
+++ 프로그래머스/even_odd.js
@@ -0,0 +1,10 @@
+//짝수와 홀수
+//정수 num이 짝수일경우 even, 홀수인 경우 odd를 반환하는 함수를 구현
+
+function solution(num) {
+    var answer = '';
+    if(num % 2 === 0 ){
+        answer = 'Even';
+    }else answer = 'Odd';
+    return answer;
+}
