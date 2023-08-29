da5158ba8cd8bcd40e0f5f4f4dd5a12a5b875d67
김지환 <jihwan-morning@mz.co.kr>
2023-08-29 10:28:29 +0900

    even odd sums

---
+++ 프로그래머스Basic/evenOddSum.js
@@ -0,0 +1,12 @@
+//홀수 짝수 더하기
+
+function solution(num_list) {
+  let even = 0;
+  let odd = 0;
+
+  num_list.map((v, idx) => {
+    !(idx % 2) ? (even += v) : (odd += v);
+  });
+
+  return odd > even ? odd : even;
+}
