b80914e28b775624c761e099c8e2a5c83de9b363
김지환 <jihwan-morning@mz.co.kr>
2023-09-16 16:41:24 +0900

    replace basic

--- 프로그래머스Basic/filterBasic2.js
+++ 프로그래머스Basic/filterBasic2.js
@@ -9,3 +9,13 @@
 function solution(intStrs, k, s, l) {
     return intStrs.map((v) => +v.slice(s, s + l)).filter((v) => v > k);
 }
+
+
+function solution(intStrs, k, s, l) {
+    return intStrs.reduce((bucket, intStr) => {
+        const v = Number(intStr.slice(s, s + l))
+        if (v > k) bucket.push(v)
+        return bucket
+    }, [])
+}
+
---
+++ 프로그래머스Basic/replaceBasic.js
@@ -0,0 +1,21 @@
+//문자 뒤집기
+
+//let a = "Progra21Sremm3";
+//let s = 6;
+//let e = 12;
+//a의 s인덱스에서 e까지 문자열 부분만 뒤집어서 바꿔준 전체 문자열을 return
+
+
+function solution(my_string, s, e) {
+    return my_string.slice(0,s) + my_string.slice(s,e + 1).split("").reverse().join("") + my_string.slice(e + 1);
+}
+
+
+function solution(my_string, s, e) {
+    var answer = '';
+    var str = my_string.substring(s,e+1);
+    var newStr = str.split('').reverse().join('');
+
+    answer = my_string.replace(str,newStr);
+    return answer;
+}
