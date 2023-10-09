c0e1c5529dffb0f5a6fcbf592dd6ca757d032d33
김지환 <jihwan-morning@mz.co.kr>
2023-10-10 03:40:43 +0900

    foreach reduce sum

---
+++ 프로그래머스Basic/forEachReduce.js
@@ -0,0 +1,40 @@
+//컨트롤 제트
+//Z앞에 오는 값들만 빼고 전부 더한 값을 return
+//s	                result
+//"1 2 Z 3"	          4
+//"10 20 30 40"	     100
+//"10 Z 20 Z 1"	      1
+
+
+
+function solution(s) {
+    const stack = []
+
+    s.split(' ').forEach((target) => {
+        if(target === 'Z') stack.pop();
+        else stack.push(+target)
+    })
+
+    return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+}
+function solution(s) {
+    s = s.split(' ');
+    let arr = [];
+    for (let v of s) v === 'Z' ? arr.length ? arr.pop() : '' : arr.push(v);
+    return arr.reduce((a,v)=>a+ +v,0);
+}
+
+function solution(s) {
+
+    let arr = s.split(" ");
+
+    while ( arr.indexOf('Z') > -1) {
+
+        arr.splice( arr.indexOf('Z')-1, 2);
+    }
+
+    return arr.reduce((a,b) => parseInt(a) + parseInt(b),0)
+}
+
+
+var solution=s=>s.split(' ').reduce((t,c)=>c=='Z'?t.slice(0,-1):[...t,c],[]).map(Number).reduce((a,b)=>a+b,0)
